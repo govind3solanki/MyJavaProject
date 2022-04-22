7a5cb95959e0903659d4d4fa03fa450de3e87088
govind solanki <solankigovind77@gmail.com>
2022-04-22 17:49:48 +0530

    updated

--- eclipse-workspace/olx-master/src/main/java/com/zensar/olxmasterapplication/controller/MasterController.java
+++ eclipse-workspace/olx-master/src/main/java/com/zensar/olxmasterapplication/controller/MasterController.java
@@ -1,8 +1,8 @@
 package com.zensar.olxmasterapplication.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,37 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zensar.olxmasterapplication.entity.Category;
 import com.zensar.olxmasterapplication.entity.Status;
+import com.zensar.olxmasterapplication.service.MasterService;
 
 @RestController
 @RequestMapping("/advertise")
 public class MasterController {
 
-	static List<Category> categorys = new ArrayList<>();
-	static {
-		categorys.add(new Category(1, "Furniture"));
-		categorys.add(new Category(2, "Cars"));
-		categorys.add(new Category(3, "Mobiles"));
-		categorys.add(new Category(4, "RealEstate"));
-		categorys.add(new Category(5, "Sports"));
-	}
-
-	static List<Status> statusList = new ArrayList<>();
-	static {
-		statusList.add(new Status(1, "OPEN"));
-		statusList.add(new Status(2, "CLOSE"));
-	}
+	@Autowired
+	private MasterService masterService;
 
 	// request 5
-	@GetMapping(value="/category", produces = { MediaType.APPLICATION_XML_VALUE,
-			MediaType.APPLICATION_JSON_VALUE })
+	@GetMapping(value = "/category", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public List<Category> getAllCategory() {
-		return categorys;
+		List<Category> allCategory = masterService.getAllCategory();
+		return allCategory;
 	}
 
 	// request 6
-	@GetMapping(value="/status", produces = { MediaType.APPLICATION_XML_VALUE,
-			MediaType.APPLICATION_JSON_VALUE })
+	@GetMapping(value = "/status", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public List<Status> getAllStatus() {
-		return statusList;
+		List<Status> allStatus = masterService.getAllStatus();
+		return allStatus;
 	}
 }
